1a5bcb74a0e62610fbf3c3b239d17bbe15df8b60
Shahzaib <zaibansari20@gmail.com>
2021-10-16 22:51:25 +0500

    Removed ENV variable Fixed logical error

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
-require('dotenv').config()
-
 const noConsole = (DEBUG = false) => {
-  const _debug = process.env.DEBUG || DEBUG
-  if (process.env.NODE_ENV === "production" || !_debug) {
+  if (process.env.NODE_ENV === "production" && !DEBUG) {
     const _console = Object.keys(console);
     _console.forEach((_c) => (console[_c] = () => {}));
   }
